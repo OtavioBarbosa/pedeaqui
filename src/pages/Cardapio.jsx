25cf3253cf854de4941a68dc55402f8e761847aa
Otávio Barbosa <otavioromualdo@gmail.com>
2021-10-17 19:49:38 -0300

    Usuários não permitidos | Permitir usuário

--- src/pages/Cardapio.jsx
+++ src/pages/Cardapio.jsx
@@ -2,53 +2,54 @@
 import React, {useEffect, useState} from "react"
 import {getPedido} from "../utils/storage"
 import api from "../services/apis"
-import {getToken} from "../services/auth"
+import {decodeToken} from "../services/auth"
 import Swal from 'sweetalert2'
 
 const Cardapio = (props) => {
 
   const [usuarios, setUsuarios] = useState([])
-  // const [nao_permitidos, setNaoPermitidos] = useState([])
+  const [nao_permitidos, setNaoPermitidos] = useState([])
+  
+  const permitirUsuario = async (pedido_has_usuario) => {
+    let acao = await Swal.fire({
+      title: 'Permitir',
+      text: `Permitir o acesso do(a) ${pedido_has_usuario.nome} à mesa?`,
+      icon: 'warning',
+      showConfirmButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não'
+    })
 
-  // const permitirUsuarios = () => {
-  //   setNaoPermitidos(usuarios.filter(u => !u.permitido && !u.encerrado && u.usuario_id === getToken()))
-    
-  //   nao_permitidos.map(async (nao_permitido) => {
-  //     await permitirUsuario(nao_permitido)
-  //     return null
-  //   })
-  // }
+    if(acao.isConfirmed){
+      await api.post(`/pedidos/usuarios/permitir/${pedido_has_usuario.id}`, {permitido: 1})
+    }
 
-  // const permitirUsuario = async (pedido_has_usuario) => {
-  //   let acao = await Swal.fire({
-  //     title: 'Permitir',
-  //     text: `Permitir o acesso do(a) ${pedido_has_usuario.nome} à mesa?`,
-  //     icon: 'warning',
-  //     showConfirmButton: true,
-  //     showCancelButton: true,
-  //     confirmButtonText: 'Sim',
-  //     cancelButtonText: 'Não'
-  //   })
-
-  //   if(acao.isConfirmed){
-  //     await Swal.fire({
-  //       title: 'Ok',
-  //       text: 'O administrador liberou seu acesso',
-  //       icon: 'success'
-  //     })
-  //     await api.post(`/pedidos/usuarios/permitir/${pedido_has_usuario.id}`, {permitido: 1})
-  //   }
-  // }
+    return null
+  }
 
   
   useEffect(() => {
-    async function getUsuarios(){
+    const getUsuarios = async () => {
       let retorno = (await api.get(`/pedidos/usuarios/${getPedido()}`)).data
       setUsuarios(retorno.data)
     }
-    
     getUsuarios()
   }, [])
+
+  useEffect(() => {
+    const usuariosNaoPermitidos = () => {
+      setNaoPermitidos(usuarios.filter(u => !u.permitido && !u.encerrado && u.usuario_id !== decodeToken()))
+    }
+    usuariosNaoPermitidos()
+  }, [usuarios])
+  
+  useEffect(() => {
+    nao_permitidos.map(async (nao_permitido) => {
+      await permitirUsuario(nao_permitido)
+      return null
+    })
+  }, [nao_permitidos])
 
   return (
     <>
